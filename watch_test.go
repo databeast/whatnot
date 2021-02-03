ad273110a4ce60bc3e5ff34b89241dfcd83d73b6
Conrad Constantine <conrad@1211.net>
2021-02-02 16:56:33 -0800

    relocate this test

--- watch_test.go
+++ watch_test.go
@@ -3,27 +3,34 @@ package whatnot
 import (
 	"testing"
 
-	"github.com/databeast/whatnot/mutex"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestWatchNotifications(t *testing.T) {
+	t.Run("Create new watch subscription on PathElement", createNewWatchOnElement)
 	t.Run("changing a path element creates notification", changeElementAndNotify)
 }
 
 func createNewWatchOnElement(t *testing.T) {
 	t.Log("creating a new watch on existing path element")
-}
-
-func changeElementAndNotify(t *testing.T) {
-	t.Log("testing that a notification is received when modifying a watched element")
-	elem := PathElement{
-		section:          SubPath("testelement"),
-		parent:           nil,
-		parentnotify:     nil,
-		mu:               &mutex.SmartMutex{},
-		subscriberNotify: nil,
+	const testNameSpace = "globaltest"
+	t.Log("Creating Namespace Manager")
+	manager = NewNamespaceManager()
+	gns := NewNamespace(testNameSpace)
+	err := manager.RegisterNamespace(gns)
+	if !assert.Nil(t, err, "RegisterNamespace returned error") {
+		return
 	}
+	testPathString := PathString("/path/to/test/data")
+	testpath := testPathString.ToAbsolutePath()
+	err = gns.RegisterAbsolutePath(testpath)
+	if !assert.Nil(t, err, "registerabsolute path returned error") {
+		t.Error(err.Error())
+		return
+	}
+
+	elem := gns.FetchAbsolutePath(testPathString)
+
 	sub := elem.SubscribeToEvents(false)
 	select {
 	case e := <-sub.Events():
@@ -31,5 +38,10 @@ func changeElementAndNotify(t *testing.T) {
 	default:
 
 	}
+
+}
+
+func changeElementAndNotify(t *testing.T) {
+	t.Log("testing that a notification is received when modifying a watched element")
 
 }
